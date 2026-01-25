e5a2e81652d4e258170c9cd2ceba33c4c426c657
Abu0907 <abusithick376@gmail.com>
2026-01-25 18:16:47 +0530

    add a palindrome code in java programming

---
+++ checkpalindrome.java
@@ -0,0 +1,23 @@
+package practice2;
+import java.util.Scanner;
+
+public class checkpalindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter a number : ");
+		int real = sc.nextInt();
+		int orginal = real;
+		int reverse = 0;
+		while(real != 0) {
+			int n = real % 10;
+			reverse = reverse * 10 + n;
+			real = real / 10;
+		}
+		if(orginal == reverse) {
+			System.out.println("given number is palindrome");
+		}
+		else {
+			System.out.println("given number is not palindrome");
+		}
+	}
+}
