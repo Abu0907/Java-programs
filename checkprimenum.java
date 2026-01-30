c5fefc12368036c887fb121759f876e7c51c058f
Abu0907 <abusithick376@gmail.com>
2026-01-30 09:24:20 +0530

    created a check prime number code in java programming

---
+++ checkprimenum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class checkprimenum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter a number:");
+		int n = sc.nextInt();
+		int m = 0;
+		for(int i=2;i<n;i++) {
+			if(n % i == 0) {
+				m++;
+			}
+		}
+		if(0 < m) {
+			System.out.println("given number is not prime number");
+		}
+		else {
+			System.out.println("given number is prime number");
+		}
+	
+	}
+}
