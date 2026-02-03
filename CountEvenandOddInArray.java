2f8e07ec16b50344c2bb31d1268f25c78ba95cb4
Abu0907 <abusithick376@gmail.com>
2026-02-03 23:43:23 +0530

    created a check and counnt odd>even numbers in java programming

---
+++ CountEvenandOddInArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountEvenandOddInArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter a size of array :");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		int even = 0, odd = 0;
+		
+		System.out.println("Enter elements with space : ");
+		for(int i=0;i<size;i++) {
+			arr[i] = sc.nextInt();
+			if(arr[i]%2 ==0) {
+				even++;
+			}
+			else {
+				odd++;
+			}
+		}
+		System.out.println("Even count : "+even+" , Odd count : "+odd);
+	}
+}
