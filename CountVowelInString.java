f694fd105fb4ad821c68199a4180a7b99588c034
Abu0907 <abusithick376@gmail.com>
2026-02-04 21:57:33 +0530

    created a count vowel in string code in java programming

---
+++ CountVowelInString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CountVowelInString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a word : ");
+		String data = sc.nextLine();
+		String duplicatedata = data.toLowerCase();
+		int vowel = 0,consonant = 0;
+		for(int i =0;i < duplicatedata.length();i++) {
+			char ch = duplicatedata.charAt(i);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'|| ch == 'u') {
+				vowel++;
+			}
+			else if('a' <= ch && ch <= 'z') {
+				consonant++;
+			}
+		}
+		System.out.println("vowels count : "+vowel+", consonant count : "+consonant);		
+	}
+}
